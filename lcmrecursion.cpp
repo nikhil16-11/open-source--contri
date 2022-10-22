933268b7ce887fc8dfd172186f3c8c44c5e7b806
Aaradhya Bansal <109024155+aaradhyabansal@users.noreply.github.com>
2022-10-22 15:04:22 +0530

    Create lcmrecursion.cpp
    Given two integers N and M, the task is to find their LCM using recursion.

---
+++ lcmrecursion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int gcd( int m,  int n)
+{
+    if (n == 0)
+        return m;
+    return gcd(n, m % n);
+}
+int lcm(int m, int n)
+{
+    return (m / gcd(m, n)) * n;
+}
+  
+ int main(void)
+{
+    cout <<"LCM of " << m << " and "<< n << " is " << lcm(m,
+n);
+return 0; }
