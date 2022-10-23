4412a9c4a7004f4b15af87c80ce89c39d841806e
MrLakshay <63330679+MrLakshay@users.noreply.github.com>
2022-10-23 17:11:56 +0530

    Check if given string have balanced Parentheses

---
+++ Valid_Parentheses.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+bool isBalanced(string& a){
+    stack<char>stk;
+    for (int i = 0; a[i]!='\0'; i++)
+    {
+        
+        char ch=a[i];
+        switch (ch)
+        {
+        case '{':
+        case '(':
+        case '[':
+            stk.push(ch);
+            break;
+        case ']':
+            if(stk.empty() || stk.top()!='['){
+                return false;
+            }
+            stk.pop();
+            break;
+        case ')':
+            if(stk.empty() || stk.top()!='('){
+                return false;
+            }
+            stk.pop();
+            break;
+        case '}':
+            if(stk.empty() || stk.top()!='{'){
+                return false;
+            }
+            stk.pop();
+            break;
+        }
+        
+    }
+    if(stk.empty()) return true;
+    return false;
+}
+int main()
+{
+    // char a[]="{a+[b*(c+d)]*e}";
+    string a;
+    cin>>a;
+    if(isBalanced(a)){
+        cout<<"Balanced"<<endl;
+    }
+    else{
+        cout<<"Not Balanced"<<endl;
+    }
+    return 0;
+}
